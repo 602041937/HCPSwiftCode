79e7aead15e3d81e094935a2df0ba266702808ff
huangpeidong <602041937@qq.com>
2020-05-29 11:53:42 +0800

    初始化项目

---
+++ HCPSwiftCode/HCPSwiftCode/HCPSwiftCode.h
@@ -0,0 +1,19 @@
+//
+//  HCPSwiftCode.h
+//  HCPSwiftCode
+//
+//  Created by huangpeidong on 2020/5/29.
+//  Copyright © 2020 HCP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HCPSwiftCode.
+FOUNDATION_EXPORT double HCPSwiftCodeVersionNumber;
+
+//! Project version string for HCPSwiftCode.
+FOUNDATION_EXPORT const unsigned char HCPSwiftCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HCPSwiftCode/PublicHeader.h>
+
+
